a3418a117db93614a50fe4c78df1bef2d47b2a21
Junhao "Jerry" Zhang <junhaodev@gmail.com>
2020-02-15 13:28:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
