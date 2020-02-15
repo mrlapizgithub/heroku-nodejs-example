89459f80563d7cc6e43cca3c1caafd9584579d27
Junhao Zhang <thepuzzlecoderuser@gmail.com>
2020-02-15 14:53:25 -0600

    Good work everyone.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
+  .use(bodyParser.urlencoded({ extended: false }))
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+  .post('/.second', (req, res) => res.send(`Success! Good work ${req.body.name}!`))
 .listen(PORT, () => console.log(`Listening on ${PORT}`));
